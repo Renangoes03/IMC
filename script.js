a6e013d43cb35d0a2215d322e675fb588eeda925
Renangoes03 <renangoessantos9@gmail.com>
2023-03-12 10:02:21 -0300

    Finalizada

--- script.js
+++ script.js
@@ -24,3 +24,15 @@
     resultado.value = "Preencha todos os campos!!!";
   }
 }
+
+const Apagar = document.getElementById("Apagar");
+const theme = window.localStorage.getItem("theme");
+
+if(theme === "dark") document.body.classList.add("dark");
+
+Apagar.addEventListener("click", () => {
+  document.body.classList.toggle("dark");
+  if (theme === "dark") {
+    window.localStorage.setItem("theme", "light");
+  }else window.localStorage.setItem("theme", "light");
+  });
